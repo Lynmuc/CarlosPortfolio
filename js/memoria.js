1a8245c33d5a8490a874d52bf713ad2c62202254
Carlos Monteiro <101756642+Lynmuc@users.noreply.github.com>
2023-08-30 13:00:00 -0300

    Add files via upload

--- js/memoria.js
+++ js/memoria.js
@@ -1,8 +1,22 @@
 const input = document.querySelector('.login-input');
 const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form')
+const validadeInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
 
-const validadeInput = (event) => {
-    
+    button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'memorygame.html';
 }
 
 input.addEventListener('input', validadeInput);
+form.addEventListener('submit', handleSubmit);
+
